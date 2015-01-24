9c5d4ad27f5db334756ed5c4df12d1b85fe48456
Inscure <drimer.eco@gmail.com>
2015-01-24 23:29:06 +0100

    Adding travis.yml

--- tests/CollectionTest.php
+++ tests/CollectionTest.php
@@ -29,4 +29,24 @@ class CollectionTest extends PHPUnit_Framework_TestCase
     {
         $this->assertSame(1, $this->array->get(0));
     }
+    
+    public function testRemoving()
+    {
+        $this->array->remove(0);
+        
+        $this->assertCount(0, $this->array);
+    }
+    
+    public function testCount()
+    {
+        $this->assertCount(1, $this->array);
+        
+        $this->array->set(0, 0);
+        
+        $this->assertCount(2, $this->array);
+        
+        $this->array->set(0, 0);
+        
+        $this->assertCount(2, $this->array);
+    }
 }
